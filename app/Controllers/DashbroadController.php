fc11ca71ceece921a03b82d279e0384638e7051a
Ishilia <ishiliagilcedeslabrador@gmail.com>
2025-04-13 15:37:28 -0400

    added the "requireLogin" method from the AuthHelper.php

--- app/Controllers/DashbroadController.php
+++ app/Controllers/DashbroadController.php
@@ -2,6 +2,9 @@
 class DashboardController {
     public function showDashboard()
     {
+        require_once __DIR__ . '/../../Helpers/AuthHelper.php';
+        requireLogin();
+        
         session_start();
         if (empty($_SESSION['user'])) {
             header('Location: /tern_application/login');
