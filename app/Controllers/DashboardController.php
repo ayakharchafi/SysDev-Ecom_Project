6739b1195ea3b121e0ba450ed664c9bfe4264c65
Ishilia <ishiliagilcedeslabrador@gmail.com>
2025-04-13 16:06:04 -0400

    changed require_once directory

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -2,7 +2,7 @@
 class DashboardController {
     public function showDashboard()
     {
-        require_once __DIR__ . '/../../Helpers/AuthHelper.php';
+        require_once __DIR__ . '/../Helpers/AuthHelper.php';
         requireLogin();
         
         session_start();
