11a2f7a6198442a6398977d6b9b1f5c3419a630c
Luke-Kn <lnwantoly@gmail.com>
2025-04-16 08:44:47 -0400

    database

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,70 @@
 <?php
 class User {
+private $id;
+private $username;
+private $password;
+private $isInternal;
+private $isSuper;
+
+    // Getters and setters
+
+    public function getID() {
+        return $this->id;
+    }
+
+    public function setID($id) {
+        $this->id = $id;
+    }    
+
+    public function getUsername() {
+        return $this->username;
+    }
+
+    public function setUsername($username) {
+        $this->username = $username;
+    }    
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+
+    public function getIsInternal() {
+        return $this->isInternal;
+    }
+
+    public function setIsInternal($isInternal) {
+        $this->isInternal = $isInternal;
+    }  
+
+    public function getIsSuper() {
+        return $this->isSuper;
+    }
+
+    public function setIsSuper($isSuper) {
+        $this->isSuper = $isSuper;
+    }  
+
     // Plain text credentials
+    public function readOne() {
+        $query = "SELECT * FROM users WHERE id = :userID";
+        $stmt = $this->dbConnection->prepare($query);
+        $stmt->bindParam(':userID', $this->id);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_CLASS, User::class);
+    }
+
+    public function readByUsername() {
+        $query = "SELECT * FROM users WHERE username = :username";
+        $stmt = $this->dbConnection->prepare($query);
+        $stmt->bindParam(':username', $this->username);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_CLASS, User::class);
+    }
+
     private $validUsers = [
         'demo' => 'demo123' // username => password
     ];
---
+++ app/core/db/dbconnectionmanager.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace database;
+
+class DBConnectionManager{
+
+    private $username;
+    private $password; // password to be stored and read through an environment variable
+    private $server; 
+    private $dbname;
+
+    private $dbConnection;
+
+    function __construct(){
+
+        $this->username = "root"; 
+        $this->password = "";
+        $this->server = "localhost";
+        $this->dbname = "TernDatabase";
+
+        try{
+        
+            $this->dbConnection = new \PDO("mysql:host=$this->server;dbname=$this->dbname", $this->username, $this->password);
+
+        }catch(\PDOException $e){
+            print "Error!: " . $e->getMessage() . "<br/>";
+        }
+    }
+
+    // could be a static function
+    function getConnection(){
+
+        return $this->dbConnection;
+
+    }   
+    
+}
