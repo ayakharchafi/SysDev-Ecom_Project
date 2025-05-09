35559b993646dd627f032a907b19362526dc4940
Nwant <nwantolyluke@gmail.com>
2025-05-09 15:04:29 -0400

    Update UserController.php

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -27,7 +27,28 @@ class UserController {
         $stmt->execute();
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
-
+    public function readRoles() {
+        $query = " SELECT users.`user_id` as user_id, `user_email`, `user_name`, `password`, external_users.client_id FROM `users`
+         left JOIN external_users ON users.user_id = external_users.user_id 
+         left JOIN internal_users ON users.user_id = internal_users.user_id 
+         ORDER BY `users`.`user_id`";
+        $stmt = $this->dbConnection->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+ 
+    public function readExternal() {
+        $query = "SELECT * FROM `users` INNER JOIN external_users ON users.user_id = external_users.user_id;";
+        $stmt = $this->dbConnection->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+    public function readInternal() {
+        $query = "SELECT * FROM `users` INNER JOIN internal_users ON users.user_id = internal_users.user_id;";
+        $stmt = $this->dbConnection->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
 
     public function displayRecords($data){
         $html = "";
@@ -37,7 +58,7 @@ class UserController {
         $html .= "<th>" . _('ID') . "</th>";
         $html .= "<th>" . _('Name') . "</th>";
         $html .= "<th>" . _('Email') . "</th>";
-        $html .= "<th>" . _('Password') . "</th>";
+        $html .= "<th>" . _('Role') . "</th>";
         $html .= "<th>" . _('Actions') . "</th>";
         $html .= "</tr>";
         $html .= "</thead>";
@@ -47,7 +68,11 @@ class UserController {
             $html .= "<td>{$user["user_id"]}</td>";
             $html .= "<td>{$user["user_name"]}</td>";
             $html .= "<td>{$user["user_email"]}</td>";
-            $html .= "<td>{$user["password"]}</td>";
+            if(isset($user["client_id"])){
+                $html .= "<td>External</td>";
+            }else{   
+                    $html .= "<td>Internal</td>";
+            }
             $html .= "   <td>";
             $html .= "   <button class= 'action-btn'><i class= 'fa-solid fa-edit'></i></button>";
             $html .= "    <button class= 'action-btn'><i class= 'fa-solid fa-trash'></i></button>";
@@ -65,7 +90,8 @@ class UserController {
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
     $users  = new UserController;
-    $data = $users->read();
+    $data = $users->readRoles();
     echo $users->displayRecords($data);
+
 }
 ?>
